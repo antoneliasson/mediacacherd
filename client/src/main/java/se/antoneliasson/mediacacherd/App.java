d14644197be0eb08a76a530d3dea63f7591d3b86
Anton Eliasson <devel@antoneliasson.se>
2014-12-14 19:54:02 +0100

    Introducing Maven

---
+++ client/src/main/java/se/antoneliasson/mediacacherd/App.java
@@ -0,0 +1,13 @@
+package se.antoneliasson.mediacacherd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
